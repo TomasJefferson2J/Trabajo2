822003e310b690a7619982c6e2a239471aa93b6c
TomasJefferson2J <1303874@senati.pe>
2023-03-10 19:00:23 -0500

    actualizacion_del_boton

--- app/src/main/java/aplicacion/app/calculadora/MainActivity.kt
+++ app/src/main/java/aplicacion/app/calculadora/MainActivity.kt
@@ -3,6 +3,7 @@ package aplicacion.app.calculadora
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,6 +12,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun presionarDigito(view: View){
+        val tv_num2: TextView = findViewById(R.id.tv_num2)
+        var num2: Double = tv_num2.text.toString().toDouble()
         
     }
 }
